afb05a0f89d2cec81f7f236999d796732e4191dc
Lake Hu <leonshark@hotmail.com>
2019-01-23 20:19:40 +0800

    refactor

--- rapp.cpp
+++ rapp.cpp
@@ -19,7 +19,7 @@ unsigned * Filter::contour(uint8_t *img, uint32_t dim, uint32_t width, uint32_t 
   uint8_t *dest = static_cast<uint8_t*>(rapp_malloc(dim * height,0));
   int ret = 0;
   //ret = rapp_pad_const_bin(img, dim, 0, width-2, height-2, 1, 0);
-  std::cout << rapp_error(ret) << std::endl;
+  //std::cout << rapp_error(ret) << std::endl;
   while((ret = rapp_contour_8conn_bin(origin,cont,1000,img, dim,width,height)) >= 0) {
     count += 1;
     //std::cout << "How many contours are found: " << ret << std::endl;
@@ -46,7 +46,7 @@ unsigned * Filter::contour(uint8_t *img, uint32_t dim, uint32_t width, uint32_t 
     origin[0] = origin[1] = 0;
   }
   rapp_free(dest);
-  std::cout << rapp_error(ret) << std::endl;
+  //std::cout << rapp_error(ret) << std::endl;
   return box;
 }
 
@@ -57,7 +57,7 @@ bool Filter::filt(int32_t sum, unsigned bwidth, unsigned bheight) {
 }
 
 ////////////////////////////////////////////////////////////////////
-Contour::Contour(uint8_t *org, uint32_t dim, uint32_t width, uint32_t height, uint8_t thresh):dim_(dim), width_(width), height_(height) {
+Contour::Contour(uint8_t *org, uint32_t dim, uint32_t width, uint32_t height):dim_(dim), width_(width), height_(height) {
   assert(org != nullptr);
 
   img_ = static_cast<uint8_t*>(rapp_malloc(dim_ * height, 0));
@@ -65,19 +65,29 @@ Contour::Contour(uint8_t *org, uint32_t dim, uint32_t width, uint32_t height, ui
   std::uninitialized_copy_n(org, dim_ * height, img_);
   std::uninitialized_fill_n(img_, dim_, 0);
   std::uninitialized_fill_n(img_+dim_*(height_-1), dim_, 0);
-  uint32_t width8 = width % 8 == 0 ? 0 : 1;
-  width8 += width / 8;
-  dim8_ = (width8 % rapp_alignment == 0 ? 0 : 1) * rapp_alignment;
-  dim8_ += width8;
-  bin_ = static_cast<uint8_t*>(rapp_malloc(dim8_ * height, 0));
-  std::cout << "dim8:" << dim8_ << "   width8:" << width8 << std::endl;
+
+  bin_ = nullptr;
+  dim8_ = 0;
+  threshold_ = 0;
+}
+
+Contour::~Contour() {
+  rapp_free(img_);
+  freeBin();
+}
+
+void Contour::thresh_gt(uint8_t thresh) {
+  alignBin();
   assert(bin_ != nullptr);
   rapp_thresh_gt_u8(bin_, dim8_, img_, dim_, width_, height_, thresh);
   threshold_ = thresh;
 }
 
-Contour::~Contour() {
-  rapp_free(img_);
+void Contour::thresh_lt(uint8_t thresh) {
+  alignBin();
+  assert(bin_ != nullptr);
+  rapp_thresh_lt_u8(bin_, dim8_, img_, dim_, width_, height_, thresh);
+  threshold_ = thresh;
 }
 
 Contour* Contour::search(Filter *filter) {
@@ -85,8 +95,9 @@ Contour* Contour::search(Filter *filter) {
   Contour *cont = nullptr;
   unsigned *box = filter->contour(bin_, dim8_, width_, height_);  //at least 4
   if(box != nullptr) {
+    std::cout << "Contour Box for Crop: " << box[0] << "," << box[1] << "," << box[2] <<","<< box[3] << std::endl;
     uint8_t *img = cropByFill(box);
-    cont = new Contour(img, dim_, width_, height_, threshold_);
+    cont = new Contour(img, dim_, width_, height_);
   }
   return cont;
 }
@@ -98,25 +109,49 @@ uint8_t* Contour::cropByFill(unsigned box[4]) {
   std::uninitialized_copy_n(img_, size, org);
 
   size = box[1] * dim_; //upper size of image
-  std::uninitialized_fill_n(org, size, 0);  // y
+  std::uninitialized_fill_n(org, size, 255);  // y
   uint32_t off = size;
   uint32_t size_right = dim_ - box[0] - box[2];
   for(uint32_t i = 0; i < box[3]; ++i) {
-    std::uninitialized_fill_n(org + off, box[0], 0);  //left part of image
-    std::uninitialized_fill_n(org + off + box[2], size_right, 0); //right part of image
+    std::uninitialized_fill_n(org + off, box[0], 255);  //left part of image
+    std::uninitialized_fill_n(org + off + box[0] + box[2], size_right, 255); //right part of image
     off += dim_; //next row
   }
   off = (box[1] + box[3]) * dim_; //lower part of image
-  std::uninitialized_fill_n(org + off, dim_ * (height_ - box[1] - box[3]), 0); //lower of y+height
+  std::uninitialized_fill_n(org + off, dim_ * (height_ - box[1] - box[3]), 255); //lower of y+height
 
   return org;
 }
 
+void Contour::freeBin() {
+  if(bin_ != nullptr)
+    rapp_free(bin_);
+  bin_ = nullptr;
+}
+
+void Contour::alignBin() {
+  freeBin();
+  uint32_t width8 = width_ % 8 == 0 ? 0 : 1;
+  width8 += width_ / 8;
+  dim8_ = (width8 % rapp_alignment == 0 ? 0 : 1) * rapp_alignment;
+  dim8_ += width8;
+  bin_ = static_cast<uint8_t*>(rapp_malloc(dim8_ * height_, 0));
+}
+
 void Contour::save(char file[]) {
-  uint8_t *buf = static_cast<uint8_t*>(rapp_malloc(dim_ * height_, 0));
   FILE *pf = fopen(file, "wb");
-  rapp_type_bin_to_u8(buf, dim_, bin_, dim8_, width_, height_);
+  fwrite(img_, 1, dim_*height_, pf);
+  fclose(pf);
+}
+
+void Contour::save_bin(char file[]) {
+  uint8_t *buf = static_cast<uint8_t*>(rapp_malloc(dim_ * height_, 0));
+  uint8_t *buf1 = static_cast<uint8_t*>(rapp_malloc(dim8_ * height_, 0));
+  rapp_bitblt_not_bin(buf1, dim8_, 0, bin_, dim8_, 0, width_, height_);
+  FILE *pf = fopen(file, "wb");
+  rapp_type_bin_to_u8(buf, dim_, buf1, dim8_, width_, height_);
   fwrite(buf, 1, dim_*height_, pf);
   fclose(pf);
   rapp_free(buf);
+  rapp_free(buf1);
 }
