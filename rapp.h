e1ca44e810831f646ebe373818f95c88e2d39222
Lake Hu <leonshark@hotmail.com>
2019-03-02 11:00:31 +0800

    some interfaces are useless.

--- rapp.h
+++ rapp.h
@@ -22,18 +22,22 @@ class Contour {
   void thresh_lt(unsigned thresh);
   int* getPacked();
   uint8_t* getData();
-  void save(char file[]);
-  void save_bin(char file[]);
+  void showU8(const char name[]);
+  void showBin(const char name[]);
+  void save(const char file[]);
+  void save_bin(const char file[]);
  private:
-  const uint32_t dim_, width_, height_;
+  const uint32_t dim_u8_, dim_bin_, rot_u8_, rot_bin_, pad_u8_, pad_bin_, off_u8_, off_bin_, width_, height_;
   uint8_t *img_, *bin_;
-  uint32_t dim8_;
+
   uint8_t threshold_;
 
+  uint8_t* crop(unsigned box[4]);
   uint8_t *cropByFill(unsigned box[4]);
-  void freeBin();
-  void alignBin();
+  //void freeBin();
+  //void alignBin();
 
+  void alignCenter(float *mapx, float *mapy, size_t srcw, size_t srch, size_t dstw, size_t dsth);
   template <typename T>
     void interp2_F(const T* const data,
                    const size_t& nrows, const size_t& ncols,
